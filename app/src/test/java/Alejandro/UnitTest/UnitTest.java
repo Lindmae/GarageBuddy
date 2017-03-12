753640fade27aa9e61f11787443335040fa04a7a
Alex <aserra23@uic.edu>
2017-03-11 23:28:46 -0600

    Creating unit test for my code

--- app/src/test/java/Alejandro/UnitTest/UnitTest.java
+++ app/src/test/java/Alejandro/UnitTest/UnitTest.java
@@ -1,8 +1,16 @@
 package Alejandro.UnitTest;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by Alex on 3/11/17.
  */
 
 public class UnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
 }
