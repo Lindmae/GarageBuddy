eb389f0a3579ebc7901444199665cc73ac532035
Alex <aserra23@uic.edu>
2017-03-13 15:46:37 -0500

    Added button

--- app/src/main/java/isabel/garagesale/MainActivity.java
+++ app/src/main/java/isabel/garagesale/MainActivity.java
@@ -2,6 +2,9 @@ package isabel.garagesale;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,32 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new MyClass() {
+
+            @Override
+            public void onClick(View v) {
+                goToSecondActivity();
+
+            }
+
+        });
     }
+
+    private void goToSecondActivity() {
+
+        Intent intent = new Intent(this, Main2Activity.class);
+
+        startActivity(intent);
+
+    }
+
+    public class MyClass implements View.OnClickListener {
+
+        @Override
+        public void onClick(View v) {
+
+        }
+    }
+
 }
