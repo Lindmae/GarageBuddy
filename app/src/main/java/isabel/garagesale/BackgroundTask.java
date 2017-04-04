e1e6b62d86853628516b950119dda391816c67ab
ccalde24 <ccalde24@uic.edu>
2017-04-04 01:02:42 -0500

    Incorporated sell data functionality, added backgroundtask for phpmyadmin connection, added myparams to pass parameters to database, proper implementation of sell data class.

---
+++ app/src/main/java/isabel/garagesale/BackgroundTask.java
@@ -0,0 +1,82 @@
+package isabel.garagesale;
+import android.os.AsyncTask;
+import android.content.Context;
+import android.widget.Toast;
+
+import java.net.HttpURLConnection;
+import java.net.*;
+import java.io.*;
+import android.util.*;
+
+/**
+ * Created by chriscalderon on 4/3/17.
+ */
+
+public class BackgroundTask extends AsyncTask<myTaskParams,Void,String> {
+
+    Context ctx;
+
+    BackgroundTask(Context ctx){
+        this.ctx = ctx;
+
+    }
+
+    @Override
+    protected String doInBackground(myTaskParams... params) {
+        String reg_url = "http:///10.0.0.58/webservice/register.php";
+        String method = params[0].getMethod();
+        if(method.equals("TestHTTP")){
+            String startTime = params[0].Params.get(0);
+            String endTime = params[0].Params.get(1);
+            String startDate = params[0].Params.get(2);
+            String endDate = params[0].Params.get(3);
+
+            try{
+                URL url = new URL(reg_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                String error;
+                if(httpURLConnection !=null){
+                    error = "connection successful";
+                    Log.d("error",error);
+                }
+                else{
+                    error = "connection unsuccessful";
+                    Log.d("error",error);
+                }
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream OS = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(OS,"UTF-8"));
+                String data = URLEncoder.encode("startTime","UTF-8")+"="+URLEncoder.encode(startTime,"UTF-8")+"&"+
+                        URLEncoder.encode("endTime","UTF-8")+"="+URLEncoder.encode(endTime,"UTF-8")+"&"+
+                        URLEncoder.encode("startDate","UTF-8")+"="+URLEncoder.encode(startDate,"UTF-8")+"&"+
+                        URLEncoder.encode("endDate","UTF-8")+"="+URLEncoder.encode(endDate,"UTF-8");
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                OS.close();
+                InputStream IS = httpURLConnection.getInputStream();
+                IS.close();
+                return "Start time added!";
+
+            } catch (MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+
+
+
+
+
+        }
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String result){
+        Toast.makeText(ctx,result, Toast.LENGTH_LONG).show();
+    }
+}
--- app/src/main/java/isabel/garagesale/Categories.java
+++ app/src/main/java/isabel/garagesale/Categories.java
@@ -5,13 +5,41 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 public class Categories extends AppCompatActivity {
+
+    CheckBox toysCheckBox;
+    CheckBox babyCheckBox;
+    CheckBox kidsCheckBox;
+    CheckBox youngACheckBox;
+    CheckBox adultCheckBox;
+    CheckBox electronicsCheckBox;
+    CheckBox sportgoodsCheckBox;
+    CheckBox toolsCheckBox;
+    CheckBox appliancesCheckBox;
+    CheckBox entCheckBox;
+    CheckBox videogamesCheckBox;
+    CheckBox miscCheckBox;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_categories);
+        toysCheckBox = (CheckBox) findViewById(R.id.toysCheckBox);
+        babyCheckBox = (CheckBox) findViewById(R.id.babyCheckBox);
+        kidsCheckBox = (CheckBox) findViewById(R.id.kidsCheckBox);
+        youngACheckBox = (CheckBox) findViewById(R.id.youngACheckBox);
+        adultCheckBox = (CheckBox) findViewById(R.id.adultCheckBox);
+        electronicsCheckBox = (CheckBox) findViewById(R.id.electronicsCheckBox);
+        sportgoodsCheckBox = (CheckBox) findViewById(R.id.sportgoodsCheckBox);
+        toolsCheckBox = (CheckBox) findViewById(R.id.toolsCheckBox);
+        appliancesCheckBox = (CheckBox) findViewById(R.id.appliancesCheckBox);
+        entCheckBox = (CheckBox) findViewById(R.id.entCheckBox);
+        videogamesCheckBox = (CheckBox) findViewById(R.id.videogamesCheckBox);
+        miscCheckBox = (CheckBox) findViewById(R.id.miscCheckBox);
+
         Button button = (Button) findViewById(R.id.saveBtn);
         button.setOnClickListener(new Categories.MyClass() {
 
@@ -24,10 +52,48 @@ public class Categories extends AppCompatActivity {
         });
     }
 
+
     private void goToSecondActivity() {
         Intent prevIntent = getIntent();
-        SellData sellData = (SellData)prevIntent.getSerializableExtra("GlobalData5");
+        SellData sellData = (SellData)prevIntent.getSerializableExtra("globalData5");
         //code to modify the SellData goes here
+
+        if(toysCheckBox.isChecked()){
+            sellData.setCategories(toysCheckBox.getText().toString());
+        }
+        if(babyCheckBox.isChecked()){
+            sellData.setCategories(babyCheckBox.getText().toString());
+        }
+        if(kidsCheckBox.isChecked()){
+            sellData.setCategories(kidsCheckBox.getText().toString());
+        }
+        if(youngACheckBox.isChecked()){
+            sellData.setCategories(youngACheckBox.getText().toString());
+        }
+        if(adultCheckBox.isChecked()){
+            sellData.setCategories(adultCheckBox.getText().toString());
+        }
+        if(electronicsCheckBox.isChecked()){
+            sellData.setCategories(electronicsCheckBox.getText().toString());
+        }
+        if(sportgoodsCheckBox.isChecked()){
+            sellData.setCategories(sportgoodsCheckBox.getText().toString());
+        }
+        if(toolsCheckBox.isChecked()){
+            sellData.setCategories(toolsCheckBox.getText().toString());
+        }
+        if(appliancesCheckBox.isChecked()){
+            sellData.setCategories(appliancesCheckBox.getText().toString());
+        }
+        if(entCheckBox.isChecked()){
+            sellData.setCategories(entCheckBox.getText().toString());
+        }
+        if(videogamesCheckBox.isChecked()){
+            sellData.setCategories(videogamesCheckBox.getText().toString());
+        }
+        if(miscCheckBox.isChecked()){
+            sellData.setCategories(miscCheckBox.getText().toString());
+        }
 
         Intent intent = new Intent(this, Description.class);
 
