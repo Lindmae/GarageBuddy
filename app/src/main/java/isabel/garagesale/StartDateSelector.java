3d930682cf501634d2228cfbaedf4dde5015d4a7
Lindmae <ilindm2@uic.edu>
2017-04-24 20:04:13 -0500

    Added error checking for date with popup message

--- app/src/main/java/isabel/garagesale/StartDateSelector.java
+++ app/src/main/java/isabel/garagesale/StartDateSelector.java
@@ -1,11 +1,19 @@
 package isabel.garagesale;
 
 import android.content.Intent;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.EditText;
+
+import java.util.Date;
+
+import static android.support.design.widget.Snackbar.LENGTH_SHORT;
+import static android.support.design.widget.Snackbar.make;
 
 public class StartDateSelector extends AppCompatActivity {
 
@@ -20,17 +28,41 @@ public class StartDateSelector extends AppCompatActivity {
         setContentView(R.layout.activity_start_date_selector);
         datePicker = (DatePicker) findViewById(R.id.datePicker);
         Button button = (Button) findViewById(R.id.button5);
+
+
         button.setOnClickListener(new StartDateSelector.MyClass() {
 
             @Override
             public void onClick(View v) {
-                goToSecondActivity();
+                if(checkValid() == true)
+                    goToSecondActivity();
+                else {
+                    CharSequence message = "Invalid date!";
+                    Snackbar mySnackbar = Snackbar.make(findViewById(R.id.myCoordinatorLayout), message, Snackbar.LENGTH_SHORT);
+                    mySnackbar.show();
+                    //Snackbar mySnackbar = Snackbar.make(R.layout.activity_start_date_selector, "Invalid date!", Snackbar.LENGTH_SHORT);
+                }
 
             }
 
         });
     }
 
+    private boolean checkValid(){
+        Date dt = new Date();
+        int currentDay = dt.getDate();
+        int currentMonth = dt.getMonth();
+        int currentYear = dt.getYear();
+
+        if (datePicker.getDayOfMonth() < currentDay)
+            return false;
+        if ((datePicker.getMonth() + 1) < currentMonth)
+            return false;
+        if (datePicker.getYear() < currentYear)
+            return false;
+
+        return true;
+    }
     private void goToSecondActivity() {
         Intent prevIntent = getIntent();
         SellData sellData = (SellData)prevIntent.getSerializableExtra("globalData3");
